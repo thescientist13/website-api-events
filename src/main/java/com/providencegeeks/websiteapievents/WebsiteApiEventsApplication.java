c1e3243dd7ec219e92efaa0ae479f257216bfa44
Owen Buckley <obuckley@kenzan.com>
2017-10-11 18:01:39 -0400

    fresh branch with fresh spring boot 2.0.0

---
+++ src/main/java/com/providencegeeks/websiteapievents/WebsiteApiEventsApplication.java
@@ -0,0 +1,12 @@
+package com.providencegeeks.websiteapievents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsiteApiEventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsiteApiEventsApplication.class, args);
+	}
+}
